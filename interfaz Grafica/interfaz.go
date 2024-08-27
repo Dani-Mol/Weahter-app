7519d1215fba1ea3d69709632a35b52c4677e685
axelit19 <axelit19@ciencias.unam.mx>
2024-08-26 22:58:40 -0600

    metodos de seleccion

--- interfaz Grafica/interfaz.go
+++ interfaz Grafica/interfaz.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
-	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -11,18 +14,97 @@ func main() {
 	myApp := app.New()
 
 	// Crear una nueva ventana
-	myWindow := myApp.NewWindow("Hola Fyne")
+	myWindow := myApp.NewWindow("Whater App")
 
-	// Crear un botón con un texto y una función que se ejecuta al ser presionado
-	helloButton := widget.NewButton("Presióname", func() {
-		myWindow.SetContent(widget.NewLabel("¡Hola Mundo!"))
+	opciones := []string{"Ticket", "Ciudad", "ITA"}
+
+	selectWidget := widget.NewSelect(opciones, func(valor string) {
+
 	})
-
-	// Establecer el contenido de la ventana como el botón
-	myWindow.SetContent(container.NewVBox(
-		helloButton,
-	))
 
 	// Mostrar la ventana
 	myWindow.ShowAndRun()
+}
+
+func seleccionarOpciones(valor string, myWindow fyne.Window) {
+	switch valor {
+	case "ITA":
+		manejarITA(myWindow)
+	case "Ticket":
+		manejarTicket(myWindow)
+	case "Ciudad":
+		manejarCiudad()
+	}
+}
+
+func manejarTicket(myWindow fyne.Window) {
+	var flujo = true
+	for flujo {
+		entrada := widget.NewEntry()
+		dialog.ShowCustomConfirm("Ingrese Ticket", "Aceptar", "Cancelar", entrada, func(confirmacion bool) {
+			if confirmacion {
+				//Aqui se manda a llamar al metodo que da los datos del csv
+				//Se pretende hacer un metodo que ingresado la entrada del usuario regrese si el Ita es correcto
+				//Si es correcto se manda a llamar un metodo  que despegue las recomendaciones
+				encontrado := false //Aqui enetrada va a ser el resultado del metodo anteriormente
+				if !encontrado {
+					fmt.Println("Ita no encontrado. Ingresa una nueva opcion por favor")
+					flujo = true
+				} else {
+					flujo = false
+				}
+			} else {
+				fmt.Println("Gracias por su visita")
+				flujo = false
+			}
+		}, myWindow)
+	}
+}
+
+func manejarITA(myWindow fyne.Window) {
+	var flujo = true
+	for flujo {
+		entrada := widget.NewEntry()
+		dialog.ShowCustomConfirm("Ingrese ITA", "Aceptar", "Cancelar", entrada, func(confirmacion bool) {
+			if confirmacion {
+				//Aqui se manda a llamar al metodo que da los datos del csv
+				//Se pretende hacer un metodo que ingresado la entrada del usuario regrese si el Ita es correcto
+				//Si es correcto se manda a llamar un metodo  que despegue las recomendaciones
+				encontrado := false //Aqui enetrada va a ser el resultado del metodo anteriormente
+				if !encontrado {
+					fmt.Println("Ita no encontrado. Ingresa una nueva opcion por favor")
+					flujo = true
+				} else {
+					flujo = false
+				}
+			} else {
+				fmt.Println("Gracias por su visita")
+				flujo = false
+			}
+		}, myWindow)
+	}
+}
+
+func manejarCiudad(myWindow fyne.Window) {
+	var flujo = true
+	for flujo {
+		entrada := widget.NewEntry()
+		dialog.ShowCustomConfirm("Ingrese Ciudad", "Aceptar", "Cancelar", entrada, func(confirmacion bool) {
+			if confirmacion {
+				//Aqui se manda a llamar al metodo que da los datos del csv
+				//Se pretende hacer un metodo que ingresado la entrada del usuario regrese si el Ita es correcto
+				//Si es correcto se manda a llamar un metodo  que despegue las recomendaciones
+				encontrado := false //Aqui enetrada va a ser el resultado del metodo anteriormente
+				if !encontrado {
+					fmt.Println("Ita no encontrado. Ingresa una nueva opcion por favor")
+					flujo = true
+				} else {
+					flujo = false
+				}
+			} else {
+				fmt.Println("Gracias por su visita")
+				flujo = false
+			}
+		}, myWindow)
+	}
 }
